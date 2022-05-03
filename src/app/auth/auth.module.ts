a5876e95da06a7e0d928c49832e560d70549c2da
VladislavOoll <apahiphip@mail.ru>
2022-05-03 19:42:38 +0300

    feat: create auth Reducer

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './components/register/register.component';
+import { StoreModule } from '@ngrx/store'
+import { reducers } from './store/reducers'
 
 const routes: Routes = [
   {
@@ -19,7 +21,8 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers)
   ]
 })
 export class AuthModule {
