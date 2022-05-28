755f114a663d37d85718dd251ffee2ce0a5dae9c
VladislavOoll <apahiphip@mail.ru>
2022-05-28 18:13:08 +0300

    feat: getUser

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -13,18 +13,22 @@ export class AuthService {
   constructor(private http: HttpClient) {
   }
 
+  getUser(response: AuthResponseInterface): CurrentUserInterface {
+    return response.user
+  }
+
   register(data: RegisterRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users'
 
     return this.http.post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user))
+      .pipe(map(this.getUser))
   }
 
   login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
     const url = environment.apiUrl + '/users/login'
 
     return this.http.post<AuthResponseInterface>(url, data)
-      .pipe(map((response: AuthResponseInterface) => response.user))
+      .pipe(map(this.getUser))
   }
 
 }
