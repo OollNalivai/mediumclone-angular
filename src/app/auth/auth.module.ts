453be543b8b640a137800a17980007fc992eab4d
VladislavOoll <apahiphip@mail.ru>
2022-05-30 22:19:02 +0300

    feat: login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,19 +11,25 @@ import { EffectsModule } from '@ngrx/effects'
 import { RegisterEffect } from './store/effects/register.effect'
 import { LoginEffect } from './store/effects/login.effect'
 import { BackendErrorMessagesModule } from '../shared/modules/backendErrorMessages/backendErrorMessages.module'
-import { PersistenceService } from '../shared/services/persistence.service'
+import { PersistenceService } from '../shared/services/persistence.service';
+import { LoginComponent } from './components/login/login.component'
 
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { Observable } from 'rxjs'
+import { BackendErrorsInterface } from '../../../shared/types/backendErrors.interface'
+import { select, Store } from '@ngrx/store'
+import { AppStateInterface } from '../../../shared/types/appState.interface'
+import { isSubmittingSelector, validationErrorsSelector } from '../../store/selectors'
+import { LoginRequestInterface } from '../../types/loginRequest.interface'
+import { loginAction } from '../../store/actions/login.action'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup;
+  isSubmitting$: Observable<boolean>
+  backendErrors$: Observable<BackendErrorsInterface | null>
+
+  constructor(private fb: FormBuilder, private store: Store<AppStateInterface>) { }
+
+  ngOnInit(): void {
+    this.initForm();
+    this.initValues();
+  }
+
+  initForm(): void {
+    this.form = this.fb.group({
+      email: '',
+      password: ''
+    });
+  }
+
+  initValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    this.backendErrors$ = this.store.pipe(select(validationErrorsSelector))
+    console.log(this.isSubmitting$, this.backendErrors$)
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+    const request: LoginRequestInterface = {
+      user: this.form.value
+    }
+    this.store.dispatch(loginAction({request}))
+  }
+
+}
