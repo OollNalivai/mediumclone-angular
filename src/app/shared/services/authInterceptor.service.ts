57cc8e3937fc6f1c1ee346d848c1bf4410c79088
VladislavOoll <apahiphip@mail.ru>
2022-06-08 00:12:53 +0300

    feat: intercept

--- src/app/shared/services/authInterceptor.service.ts
+++ src/app/shared/services/authInterceptor.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core'
-import { HttpInterceptor } from '@angular/common/http'
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http'
+import { PersistenceService } from './persistence.service'
+import { Observable } from 'rxjs'
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(private persistenceService: PersistenceService) {
   }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // req = request.clone({
-    //
-    // })
-    return undefined;
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.persistenceService.get('accessToken');
+    request = request.clone({
+      setHeaders: {
+        Authorization: token ? `Token ${token}` : ''
+      }
+    })
+    return next.handle(request)
   }
-
-
 
 
 }
