100099167105343605565d211c1324b18922c92f
VladislavOoll <apahiphip@mail.ru>
2022-06-13 23:48:37 +0300

    fix: imports global feed

--- src/app/globalFeed/components/global-feed/global-feed.component.ts
+++ src/app/globalFeed/components/global-feed/global-feed.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-global-feed',
+  selector: 'mc-global-feed',
   templateUrl: './global-feed.component.html',
   styleUrls: ['./global-feed.component.scss']
 })
--- src/app/globalFeed/globalFeed.module.ts
+++ src/app/globalFeed/globalFeed.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common';
+import { GlobalFeedComponent } from './components/global-feed/global-feed.component'
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [GlobalFeedComponent]
 })
 
 export class GlobalFeedModule {
