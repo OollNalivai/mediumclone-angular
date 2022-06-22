469dcdb093a126aa895666c71cdbefb8a68c6315
VladislavOoll <apahiphip@mail.ru>
2022-06-22 23:38:51 +0300

    feat: get feed effect

--- src/app/shared/modules/feed/components/feed.module.ts
+++ src/app/shared/modules/feed/components/feed.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FeedComponent } from './feed/feed.component'
+import { EffectsModule } from '@ngrx/effects'
+import { GetFeedEffect } from './feed/store/effects/getFeed.effect'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
   declarations: [FeedComponent],
   exports: [FeedComponent]
 })
