74dead0f99439a9b59edb6d32932c8db04673abf
VladislavOoll <apahiphip@mail.ru>
2022-06-25 18:35:52 +0300

    feat: feed Reducer get feed success and failure actions

--- src/app/shared/modules/feed/components/feed.module.ts
+++ src/app/shared/modules/feed/components/feed.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common'
 import { FeedComponent } from './feed/feed.component'
 import { EffectsModule } from '@ngrx/effects'
 import { GetFeedEffect } from './feed/store/effects/getFeed.effect'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from './feed/store/reducers'
+import { FeedService } from './feed/services/feed.service'
 
 @NgModule({
-  imports: [CommonModule, EffectsModule.forFeature([GetFeedEffect])],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetFeedEffect]),
+    StoreModule.forFeature('feed', reducers)],
   declarations: [FeedComponent],
-  exports: [FeedComponent]
+  exports: [FeedComponent],
+  providers: [FeedService]
 })
 
 export class FeedModule {
--- src/app/shared/modules/feed/components/feed/store/reducers.ts
+++ src/app/shared/modules/feed/components/feed/store/reducers.ts
@@ -1,6 +1,6 @@
 import { FeedStateInterface } from '../types/feedState.interface'
-import { createReducer, on } from '@ngrx/store'
-import { getFeedAction } from './actions/getFeed.action'
+import { Action, createReducer, on } from '@ngrx/store'
+import { getFeedAction, getFeedFailureAction, getFeedSuccessAction } from './actions/getFeed.action'
 
 const initialState: FeedStateInterface = {
   isLoading: false,
@@ -16,5 +16,25 @@ const feedReducer = createReducer(
       ...state,
       isLoading: true
     })
+  ),
+  on(
+    getFeedSuccessAction,
+    (state, action): FeedStateInterface => ({
+      ...state,
+      isLoading: true,
+      data: action.feed
+    })
+  ),
+  on(
+    getFeedFailureAction,
+    (state): FeedStateInterface => ({
+      ...state,
+      isLoading: false
+    })
   )
 )
+
+
+export function reducers(state: FeedStateInterface, action: Action) {
+  return feedReducer(state, action)
+}
