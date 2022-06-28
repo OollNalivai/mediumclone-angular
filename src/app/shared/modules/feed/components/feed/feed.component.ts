31d9a6360c808e00b64d8640a18c3eb189729520
VladislavOoll <apahiphip@mail.ru>
2022-06-28 22:40:49 +0300

    feat: initializeValues and fetchData functions

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { select, Store } from '@ngrx/store'
 import { getFeedAction } from './store/actions/getFeed.action'
+import { Observable } from 'rxjs'
+import { GetFeedResponseInterface } from './types/getFeedResponse.interface'
+import { errorSelector, feedSelector, isLoadingSelector } from './store/selectors'
 
 @Component({
   selector: 'mc-feed',
@@ -11,10 +14,24 @@ export class FeedComponent implements OnInit {
 
   @Input('apiUrl') apiUrlProps: string;
 
+  isLoading$: Observable<boolean>
+  error$: Observable<string | null>
+  feed$: Observable<GetFeedResponseInterface | null>
+
   constructor(private store: Store) { }
 
   ngOnInit(): void {
-    this.store.dispatch(getFeedAction({url: this.apiUrlProps}))
+    this.initializeValues()
+    this.fetchData()
   }
 
+  initializeValues(): void {
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+    this.feed$ = this.store.pipe(select(feedSelector));
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getFeedAction({url: this.apiUrlProps}))
+  }
 }
