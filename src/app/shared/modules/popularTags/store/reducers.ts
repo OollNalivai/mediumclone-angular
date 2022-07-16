8f30aa8de7b107d5789579beefc9da2790b0156d
VladislavOoll <apahiphip@mail.ru>
2022-07-16 23:22:08 +0300

    feat: popular tags Reducer

--- src/app/shared/modules/popularTags/store/reducers.ts
+++ src/app/shared/modules/popularTags/store/reducers.ts
@@ -1,7 +1,39 @@
 import { PopularTagsStateInterface } from '../types/popularTagsState.interface'
+import { createReducer, on } from '@ngrx/store'
+import {
+  getPopularTagsAction,
+  getPopularTagsFailureAction,
+  getPopularTagsSuccessAction
+} from './actions/getPopularTags.action'
 
 const initialState: PopularTagsStateInterface = {
   data: null,
   isLoading: false,
   error: null
 }
+
+const popularTagsReducer = createReducer(
+  initialState,
+  on(
+    getPopularTagsAction,
+    (state): PopularTagsStateInterface => ({
+      ...state,
+      isLoading: true
+    }),
+  ),
+  on(
+    getPopularTagsSuccessAction,
+    (state, action): PopularTagsStateInterface => ({
+      ...state,
+      isLoading: false,
+      data: action.popularTags
+    })
+  ),
+  on(
+    getPopularTagsFailureAction,
+    (state): PopularTagsStateInterface => ({
+      ...state,
+      isLoading: false
+    })
+  )
+)
