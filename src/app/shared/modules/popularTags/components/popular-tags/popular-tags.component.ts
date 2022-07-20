9fbdf9148d2047ae7bf0d13693884e0790582fe9
VladislavOoll <apahiphip@mail.ru>
2022-07-20 22:22:55 +0300

    feat: initialize values from PopularTagsComponent, fetchData

--- src/app/shared/modules/popularTags/components/popular-tags/popular-tags.component.ts
+++ src/app/shared/modules/popularTags/components/popular-tags/popular-tags.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store'
+import { select, Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { PopularTagType } from '../../../../types/popularTag.type'
+import { getPopularTagsAction } from '../../store/actions/getPopularTags.action'
+import { errorSelector, isLoadingSelector, popularTagsSelector } from '../../store/selectors'
 
 @Component({
   selector: 'mc-popular-tags',
@@ -7,6 +11,11 @@ import { Store } from '@ngrx/store'
   styleUrls: ['./popular-tags.component.scss']
 })
 export class PopularTagsComponent implements OnInit {
+
+  popularTags$: Observable<PopularTagType[] | null>
+  isLoading$: Observable<boolean>
+  error$: Observable<string | null>
+
 
   constructor(private store: Store) { }
 
@@ -16,11 +25,13 @@ export class PopularTagsComponent implements OnInit {
   }
 
   initializeValues() {
-
+    this.popularTags$ = this.store.pipe(select(popularTagsSelector))
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
   }
 
   fetchData() {
-
+    this.store.dispatch(getPopularTagsAction())
   }
 
 }
