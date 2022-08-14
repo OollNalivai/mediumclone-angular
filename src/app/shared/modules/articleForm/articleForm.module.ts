77743e668d25395c0892c8ea8ab26ab172b76280
VladislavOoll <apahiphip@mail.ru>
2022-08-14 23:57:17 +0300

    feat: FormGroup for article submit

--- src/app/shared/modules/articleForm/articleForm.module.ts
+++ src/app/shared/modules/articleForm/articleForm.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ArticleFormComponent } from './components/article-form/article-form.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [ArticleFormComponent],
   exports: [ArticleFormComponent]
 })
--- src/app/shared/modules/articleForm/components/article-form/article-form.component.ts
+++ src/app/shared/modules/articleForm/components/article-form/article-form.component.ts
@@ -1,15 +1,40 @@
-import { Component, Input } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { BackendErrorsInterface } from '../../../../types/backendErrors.interface'
 import { ArticleInputInterface } from '../../../../types/articleInput.interface'
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'mc-article-form',
   templateUrl: './article-form.component.html',
   styleUrls: ['./article-form.component.scss']
 })
-export class ArticleFormComponent {
+export class ArticleFormComponent implements OnInit {
 
   @Input('initialValues') initialValuesProps: ArticleInputInterface
   @Input('isSubmitting') isSubmittingProps: boolean
   @Input('errors') errorsProps: BackendErrorsInterface | null
+
+  @Output('articleSubmit') articleSubmitEvent = new EventEmitter<ArticleInputInterface>()
+
+  form: FormGroup
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  private initializeForm(): void {
+    this.form = this.fb.group({
+      title: '',
+      description: '',
+      body: '',
+      tagList: ''
+    })
+  }
+
+  onSubmit(): void {
+    this.articleSubmitEvent.emit(this.form.value)
+  }
 }
