abf6c4b3b37dca7b0092e3db2d79f6a4d76c4941
VladislavOoll <apahiphip@mail.ru>
2022-08-28 20:54:49 +0300

    feat: get article effect

--- src/app/aditArticle/editArticle.module.ts
+++ src/app/aditArticle/editArticle.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core'
 import { EditArticleService } from './services/editArticle.service'
 import { ArticleService as SharedArticleService } from '../shared/services/article.service'
-
+import { CommonModule } from '@angular/common'
+import { EffectsModule } from '@ngrx/effects'
+import { GetArticleEffect } from './store/effects/getArticle.effect'
+import { UpdateArticleEffect } from './store/effects/updateArticle.effect'
 
 @NgModule({
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetArticleEffect, UpdateArticleEffect]),
+  ],
+  declarations: [
+
+  ],
   providers: [
     EditArticleService,
     SharedArticleService
