ccdd83f7ba9b49e2b1d929b56192b89c04b53ad3
VladislavOoll <apahiphip@mail.ru>
2022-09-22 22:30:10 +0300

    feat: initializeValues for userProfile

--- src/app/userProfile/components/user-profile/user-profile.component.ts
+++ src/app/userProfile/components/user-profile/user-profile.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ProfileInterface } from '../../../shared/types/profile.interface'
+import { Observable, Subscription } from 'rxjs'
+import { select, Store } from '@ngrx/store'
+import { ActivatedRoute, Router } from '@angular/router'
+import { getUserProfileAction } from '../../store/actions/getUserProfile.action'
+import { errorSelector, isLoadingSelector } from '../../store/selectors'
 
 @Component({
   selector: 'mc-user-profile',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  userProfile: ProfileInterface
+  isLoading$: Observable<boolean>
+  error$: Observable<string | null>
+  userProfileSubscription: Subscription
+  slug: string
+  apiUrl: string
+
+  constructor(private store: Store, private route: ActivatedRoute, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.initializeValues()
+    this.fetchData()
+  }
+
+  initializeValues(): void {
+    const isFavorites = this.router.url.includes('favorites')
+    this.slug = this.route.snapshot.paramMap.get('slug')
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector))
+    this.error$ = this.store.pipe(select(errorSelector))
+    this.apiUrl = isFavorites
+      ? `/articles?favorited=${this.slug}`
+      : `/articles?author=${this.slug}`
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getUserProfileAction({ slug: this.slug }))
   }
 
 }
